00dc807795e8023e3554ecea1e8f21cab1f6da47
3str-webdev <3str.webdev@gmail.com>
2024-06-16 10:45:23 +0300

    fix: disable problem auth decorator

--- server/src/auth/decorators/auth.decorator.ts
+++ server/src/auth/decorators/auth.decorator.ts
@@ -2,10 +2,12 @@ import { ExecutionContext, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { Role } from "@prisma/client";
 
-export const Auth = (role: Role) =>
-	UseGuards(AuthGuard("jwt"), (_, ctx: ExecutionContext) => {
-		const request = ctx.switchToHttp().getRequest();
-		const user = request.user;
+export const Auth = (role: Role) => UseGuards(AuthGuard("jwt"));
 
-		return user.role === role || user.role === Role.Admin;
-	});
+// export const Auth = (role: Role) =>
+// 	UseGuards(AuthGuard("jwt"), (_, ctx: ExecutionContext) => {
+// 		const request = ctx.switchToHttp().getRequest();
+// 		const user = request.user;
+
+// 		return user.role === role || user.role === Role.Admin;
+// 	});
