4b97de8964df73ac651bedc46cf2f6046bf7940d
3str-webdev <3str.webdev@gmail.com>
2024-06-16 10:52:20 +0300

    feat: add auth guarding for category controller

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,27 +1,33 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UsePipes, ValidationPipe } from "@nestjs/common";
 import { CategoryService } from "./category.service";
 import { CategoryDto } from "./dto/category.dto";
+import { Auth } from "src/auth/decorators/auth.decorator";
+import { Role } from "@prisma/client";
 
 @Controller("categories")
 export class CategoryController {
 	constructor(private readonly categoryService: CategoryService) {}
 
+  @Auth(Role.User)
 	@Get()
 	async getAll() {
 		return this.categoryService.getAll();
 	}
 
+  @Auth(Role.User)
   @Get("by-id/:id")
   async getById(@Param("id") id: string) {
     return this.categoryService.getById(id)
   }
 
+  @Auth(Role.User)
   @Get("by-slug/:slug")
   async getBySlug(@Param("slug") slug: string) {
     return this.categoryService.getBySlug(slug)
   }
 
   @HttpCode(200)
+  @Auth(Role.Admin)
   @Post()
   async create() {
     return this.categoryService.create()
@@ -29,12 +35,14 @@ export class CategoryController {
 
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
-  @Put(":id") 
+  @Auth(Role.Admin)
+  @Put(":id")
   async update(@Param("id") id: string, @Body() dto: CategoryDto) {
     return this.categoryService.update(id, dto)
   }
 
   @HttpCode(200)
+  @Auth(Role.Admin)
   @Delete(":id")
   async delete(@Param("id") id: string) {
     return this.categoryService.delete(id)
